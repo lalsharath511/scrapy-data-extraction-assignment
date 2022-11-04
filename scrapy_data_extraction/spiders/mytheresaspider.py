d8d7c85cb0a05b6d76fffdc31488219cb68457dd
lalsharath511 <lalsharath511@gmail.com>
2022-11-04 05:21:01 -0400

    fetching data from urls in progress

--- scrapy_data_extraction/spiders/mytheresaspider.py
+++ scrapy_data_extraction/spiders/mytheresaspider.py
@@ -10,18 +10,33 @@ class MytheresaSpider(scrapy.Spider):
                yield response.follow(link.get(), callback=self.parse_products)
             except:
                 continue
-            # yield {
-            #     'link':link.get()
-            # }
-        next_page=response.css('li.next').css('a').attrib['href']
-        if next_page is not None:
-            yield response.follow(next_page, callback=self.parse)
+          
+        # next_page=response.css('li.next').css('a').attrib['href']
+        # if next_page is not None:
+        #     yield response.follow(next_page, callback=self.parse)
     def parse_products(self, response):
         product = response.css('div.main')
+        # sizes = response.css('ul.sizes span::text').getall()
+        # for size in sizes:
+        #     size.strip(" ")
+        
+        # yield {
+        #     'brand':product.css('a.text-000000::text').get(),
+        #     'product_name':product.css('span.pb2.pa1-rmm-name::text').get(),
+        #     'listing_price':product.css('span.price::text').get(),
+        #     'product_id':product.css('span.h1::text').get()
+        # }
         yield {
-            'brand':product.css('a.text-000000::text').get(),
-            'product_name':product.css('span.pb2.pa1-rmm-name::text').get(),
-            'listing_price':product.css('span.price::text').get(),
-            'product_id':product.css('span.h1::text').get()
-        }
+                "breadcrumbs":response.css('div.breadcrumbs span::text').getall(),
+                "image_url":response.css('img.gallery-image ::attr(src)').get(),
+                "brand": product.css('a.text-000000::text').get(),
+                "product_name": product.css('span.pb2.pa1-rmm-name::text').get(),
+                "listing_price": product.css('span.price::text').get(),
+                "offer_price": "€ 559",
+                "discount": "30% off",
+                "product_id": product.css('span.h1::text').get(),
+                "sizes": response.css('ul.sizes span::text').getall(),
+                "description":product.css('span.h1::text').get(),
+                "other_images":response.css('img.gallery-image ::attr(data-src)').getall()
+                }
 
