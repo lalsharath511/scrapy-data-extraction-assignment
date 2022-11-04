f46b680aff2dae43cab5b343f857f492a46af432
lalsharath511 <lalsharath511@gmail.com>
2022-11-04 15:49:28 -0400

    cleaning data

--- scrapy_data_extraction/spiders/mytheresaspider.py
+++ scrapy_data_extraction/spiders/mytheresaspider.py
@@ -1,4 +1,5 @@
 import scrapy
+import time
 
 class MytheresaSpider(scrapy.Spider):
     name = 'mytheresa'
@@ -10,33 +11,51 @@ class MytheresaSpider(scrapy.Spider):
                yield response.follow(link.get(), callback=self.parse_products)
             except:
                 continue
-          
-        # next_page=response.css('li.next').css('a').attrib['href']
-        # if next_page is not None:
-        #     yield response.follow(next_page, callback=self.parse)
+        time.sleep(10) 
+        next_page=response.css('li.next').css('a').attrib['href']
+        if next_page is not None:
+            yield response.follow(next_page, callback=self.parse)
     def parse_products(self, response):
         product = response.css('div.main')
+        a=response.css('li.pa1-rmm::text').getall()
+        a=' '.join(a)
+        b=str(product.css('p.pa1-rmm.product-description::text').get())
+        description=b+a
+        
+
+
+
         # sizes = response.css('ul.sizes span::text').getall()
         # for size in sizes:
         #     size.strip(" ")
-        
-        # yield {
-        #     'brand':product.css('a.text-000000::text').get(),
-        #     'product_name':product.css('span.pb2.pa1-rmm-name::text').get(),
-        #     'listing_price':product.css('span.price::text').get(),
-        #     'product_id':product.css('span.h1::text').get()
-        # }
+        breadcrumbs=response.css('div.breadcrumbs span::text').getall()
+        sizes_details=response.css('ul.sizes span::text').getall()
+        sizes=[]
+
+        for size in sizes_details:
+            size=size.strip(" ")
+            size=size.split("/")
+            if size[1]=="":
+                sizes.append(size[0].strip(" "))
+            else:
+                for i in size:
+                    sizes.append(i.strip(" "))
+        listing_price=response.css('span.regular-price span.price::text').get()
+        if listing_price==None:
+            listing_price=response.css('p.old-price span.price::text').get() 
+       
+
         yield {
-                "breadcrumbs":response.css('div.breadcrumbs span::text').getall(),
-                "image_url":response.css('img.gallery-image ::attr(src)').get(),
+                "breadcrumbs":breadcrumbs[0:-1],
+                "image_url":f"https:{response.css('img.gallery-image ::attr(src)').get()}",
                 "brand": product.css('a.text-000000::text').get(),
                 "product_name": product.css('span.pb2.pa1-rmm-name::text').get(),
-                "listing_price": product.css('span.price::text').get(),
-                "offer_price": "€ 559",
-                "discount": "30% off",
-                "product_id": product.css('span.h1::text').get(),
-                "sizes": response.css('ul.sizes span::text').getall(),
-                "description":product.css('span.h1::text').get(),
+                "listing_price": listing_price,
+                "offer_price": response.css('p.special-price span.price::text').get(),
+                "discount": product.css('span.price-reduction-notice::text').get(),
+                "product_id": response.css('span.h1::text').get()[-9:],
+                "sizes": sizes,
+                "description":description,
                 "other_images":response.css('img.gallery-image ::attr(data-src)').getall()
                 }
 
