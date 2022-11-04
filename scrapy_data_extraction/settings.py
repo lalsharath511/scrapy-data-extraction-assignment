becb19782405880de91ca5fe8e8d8e2526d83653
lalsharath511 <lalsharath511@gmail.com>
2022-11-04 02:50:30 -0400

    fetching data from urls in progress

--- scrapy_data_extraction/settings.py
+++ scrapy_data_extraction/settings.py
@@ -17,7 +17,7 @@ NEWSPIDER_MODULE = 'scrapy_data_extraction.spiders'
 #USER_AGENT = 'scrapy_data_extraction (+http://www.yourdomain.com)'
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = True
+ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 #CONCURRENT_REQUESTS = 32
--- scrapy_data_extraction/spiders/mytheresaspider.py
+++ scrapy_data_extraction/spiders/mytheresaspider.py
@@ -5,10 +5,23 @@ class MytheresaSpider(scrapy.Spider):
     start_urls = ['https://www.mytheresa.com/int_en/men/shoes.html']
 
     def parse(self, response):
-        for products in response.css('li.item'):
-            yield{
-                'link': products.css('a.product-image').attrib['href'],
-            }
+        for link in response.css('li.item a::attr(href)'):
+            try:
+               yield response.follow(link.get(), callback=self.parse_products)
+            except:
+                continue
+            # yield {
+            #     'link':link.get()
+            # }
         next_page=response.css('li.next').css('a').attrib['href']
         if next_page is not None:
             yield response.follow(next_page, callback=self.parse)
+    def parse_products(self, response):
+        product = response.css('div.main')
+        yield {
+            'brand':product.css('a.text-000000::text').get(),
+            'product_name':product.css('span.pb2.pa1-rmm-name::text').get(),
+            'listing_price':product.css('span.price::text').get(),
+            'product_id':product.css('span.h1::text').get()
+        }
+
